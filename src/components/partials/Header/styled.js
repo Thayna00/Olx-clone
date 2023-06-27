4a720a7181f6ded5549fece912f7ba76c43038d9
Thayna00 <oppakino5@hotmail.com>
2023-06-27 20:25:43 -0300

    Criando o Header (1- estrutura e logo)

--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,7 +1,35 @@
 import styled  from "styled-components";
 
 const HeaderArea = styled.div`
-    heigth:300px;
-    background-color:#CCC;
+    background-color:#FFF;
+    height:60px;
+    border-botton:1px solid #CCC;
+
+    .container {
+        max-width:1000px;
+        margin:auto;
+        display:flex;
+    }
+
+    a {
+        text-decoration:none;
+    }
+
+    .logo{
+        flex:1;
+        display:flex;
+        align-items:center;
+        height:60px;
+
+        .logo-1,
+        .logo-2,
+        .logo-3 {
+            font-size:27px;
+            font-weight:bold;
+        }
+        .logo-1 { color:#FF0000; }
+        .logo-2 { color:#00FF00; }
+        .logo-3 { color:#0000FF; }
+    }
 `;
 export default HeaderArea;
